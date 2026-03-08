d56306c7fd68a95c33150bf6f245dbe47ef0c5fd
Link4Electronics <link4electronics@gmail.com>
2026-03-07 21:16:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,7 +29,11 @@ pacman -Syu --noconfirm \
     lib32-libxinerama   \
     lib32-libxrandr     \
     lib32-libxrender    \
-    lib32-pango
+    lib32-pango         \
+    lib32-pipewire-jack \
+    lib32-pipewire      \
+    pipewire-audio      \
+    pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
